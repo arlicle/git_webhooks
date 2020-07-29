718e52972bdddbe2d7653b9c292a6bb519f94e8b
edison <arlicle@gmail.com>
2020-07-29 11:52:06 +0800

    fix *

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{post, web, App, HttpServer, HttpResponse};
+use actix_web::{get, post, web, App, HttpServer, HttpResponse};
 
 use serde_json::{json, Map, Value};
 
@@ -12,10 +12,17 @@ async fn git_post_receive(request_body: web::Json<Value>) -> HttpResponse {
     HttpResponse::Ok().body("done")
 }
 
+#[get("/")]
+async fn index() -> HttpResponse {
+
+    HttpResponse::Ok().body("hello")
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(git_post_receive))
+    HttpServer::new(|| App::new()
+        .service(git_post_receive)
+        .service(index))
         .bind("0.0.0.0:9005")?
         .run()
         .await
