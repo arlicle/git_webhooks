f245205d9942b298f7d55fecc3f2b2ec06d92a6d
edison <arlicle@gmail.com>
2020-07-30 14:57:26 +0800

    Fix *

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 use actix_web::{get, post, web, App, HttpRequest, HttpServer, HttpResponse};
 
 use serde_json::{json, Map, Value};
-
+use hex_literal::hex;
+use sha1::{Sha1, Digest};
 
 
 
 #[post("/git_post_receive")]
 async fn git_post_receive(req: HttpRequest, request_body: web::Json<Value>) -> HttpResponse {
     println!("REQ: {:?}", req);
+    println!("REQ: {:?}", req.headers());
     println!("request_body:\n{:?}", request_body);
+
+//    let mut hasher = Sha1::new();
+//    hasher.update(request_body.as_str().unwrap().as_bytes());
+    let s = Sha1::digest(request_body.as_str().unwrap().as_bytes());
+    println!("{:?}", s);
 
     HttpResponse::Ok().body("done")
 }
