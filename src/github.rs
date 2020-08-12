6524e396c9e5008d90b8bde18dc722aa4464820f
edison <arlicle@gmail.com>
2020-08-12 11:05:57 +0800

    Fix *

--- src/github.rs
+++ src/github.rs
@@ -51,6 +51,7 @@ pub async fn webhooks_handle(req: HttpRequest, request_body_bytes: Bytes, query_
     }
 
     println!("request_body {:?}", request_body);
+    println!("request_body {:?}", request_body.pointer("/repository/name"));
     // 获取repository name
     let repository_name: &str = match request_body.pointer("/repository/name") {
         Some(Value::String(v)) => v,
